abf3b22136b1bf30bd59807789b3cef3891eb7e5
ACMH1 <icerimmedsteel@gmail.com>
2018-03-11 03:21:27 +0800

    Added DAO for sheet class.

--- ExcelFactory.py
+++ ExcelFactory.py
@@ -12,15 +12,36 @@ class SheetDAOFactory:
     def __init__(self, file_name):
         self.file_name = file_name
         try:
-            self.wb = openpyxl.load_workbook(file_name)
+            self.workbook = openpyxl.load_workbook(file_name)
+            self.sheet_list = self.workbook.get_sheet_names()
         except FileNotFoundError:
-            self.wb = openpyxl.Workbook()
+            self.workbbook = openpyxl.Workbook()
         
     def save(self):
-        self.wb.save(self.file_name)
+        self.workbook.save(self.file_name)
     
     def create_sheet_DAO(self, sheet_name, *headers):
         pass
     
     def get_sheet_DAO(self, sheet_name):
+        pass
+    
+class SheetDAOImpl:
+    
+    def __init__(self, worksheet):
+        self.worksheet = worksheet
+        self.column_headers = []
+        for column in self.worksheet.itercols():
+            self.column_headers.append(column[0].value)
+    
+    def create_row(self, row):
+        pass
+    
+    def find_row(self, row):
+        pass
+    
+    def update_row(self, row):
+        pass
+    
+    def delete_row(self, row):
         pass
